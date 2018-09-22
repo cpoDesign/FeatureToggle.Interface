d453c152df9a7e97ef70a028d7a6602abf646e74
pav <pvl.svarc@gmail.com>
2018-09-22 21:11:11 +0100

    Initial add on

---
+++ IFeature.cs
@@ -0,0 +1,12 @@
+﻿namespace FeatureToggle.Interface
+{
+    public interface IFeature
+    {
+        /// <summary>
+        /// Boolean result to determine if feature is enabled or not
+        /// </summary>
+        /// <param name="feature">Name of the feature</param>
+        /// <returns>Boolean result</returns>
+        bool IsFeatureEnabled(string feature);
+    }
+}
